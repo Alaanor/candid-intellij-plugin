40680b56f04483eababcda1a161cd90b2e5c45ba
GitHub Action <action@github.com>
2022-03-05 18:32:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/alaanor/candidintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.alaanor.candidintellijplugin.services
+
+import com.github.alaanor.candidintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
