d67cc372accfe3fe8021f82d0f1a84e1a6dd2f02
Maxime Bonvin <max.bonvin@gmail.com>
2022-03-31 20:50:11 +0200

    some naming

--- src/main/kotlin/com/github/alaanor/candid/psi/CandidTokenType.kt
+++ src/main/kotlin/com/github/alaanor/candid/psi/CandidTokenType.kt
@@ -3,8 +3,4 @@ package com.github.alaanor.candid.psi
 import com.github.alaanor.candid.CandidLanguage
 import com.intellij.psi.tree.IElementType
 
-class CandidTokenType(debugName: String) : IElementType(debugName, CandidLanguage.INSTANCE) {
-    override fun toString(): String {
-        return "CandidTokenType.${super.toString()}"
-    }
-}
+class CandidTokenType(debugName: String) : IElementType(debugName, CandidLanguage.INSTANCE)
