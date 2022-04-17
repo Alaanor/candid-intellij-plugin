10bf9a87033b5485a8790d77fa581240f820c3a0
Maxime Bonvin <max.bonvin@gmail.com>
2022-04-17 11:51:27 +0200

    resolve import statement

--- src/main/kotlin/com/github/alaanor/candid/reference/CandidImportReference.kt
+++ src/main/kotlin/com/github/alaanor/candid/reference/CandidImportReference.kt
@@ -3,12 +3,19 @@ package com.github.alaanor.candid.reference
 import com.github.alaanor.candid.psi.CandidImportStatement
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiReferenceBase
 
 class CandidImportReference(importStatement: CandidImportStatement, textRange: TextRange) :
     PsiReferenceBase<CandidImportStatement>(importStatement, textRange) {
 
     override fun resolve(): PsiElement? {
-        return null
+        if (element.stringLiteral == null) {
+            return null
+        }
+
+        val targetName = element.stringLiteral!!.text.trim('"')
+        val target = element.containingFile.virtualFile.findFileByRelativePath("../$targetName")
+        return target?.let { PsiManager.getInstance(element.project).findFile(it) }
     }
 }
