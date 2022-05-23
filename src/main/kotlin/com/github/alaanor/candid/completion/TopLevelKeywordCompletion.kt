755039b9c3a6daa66cee44b53a2b273fb881433c
Maxime Bonvin <max.bonvin@gmail.com>
2022-05-23 19:24:05 +0200

    no top level keyword suggestion inside an actor

--- src/main/kotlin/com/github/alaanor/candid/completion/TopLevelKeywordCompletion.kt
+++ src/main/kotlin/com/github/alaanor/candid/completion/TopLevelKeywordCompletion.kt
@@ -1,18 +1,24 @@
 package com.github.alaanor.candid.completion
 
+import com.github.alaanor.candid.psi.CandidActor
 import com.github.alaanor.candid.psi.CandidFile
 import com.intellij.codeInsight.completion.InsertHandler
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.patterns.ElementPattern
-import com.intellij.patterns.PlatformPatterns
+import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiErrorElement
 
 class TopLevelKeywordCompletion : CandidBasicCompletion() {
-    override val elementPattern: ElementPattern<out PsiElement> = PlatformPatterns.psiElement()
-        .andNot(PlatformPatterns.psiElement().beforeLeaf(";"))
+    override val elementPattern: ElementPattern<out PsiElement> = psiElement()
+        .andNot(psiElement().beforeLeaf(";"))
         .withParent(PsiErrorElement::class.java)
-        .withSuperParent(2, CandidFile::class.java)
+        .withSuperParent(
+            2,
+            psiElement(CandidFile::class.java).andNot(
+                psiElement().withChild(psiElement(CandidActor::class.java))
+            )
+        )
 
     override val tokenList: List<String> = listOf("type", "service", "import")
 
