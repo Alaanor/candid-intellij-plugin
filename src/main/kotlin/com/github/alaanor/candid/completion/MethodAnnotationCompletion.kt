f06cea446882fc737f27b959fc1400f7bbb286b3
Maxime Bonvin <max.bonvin@gmail.com>
2022-05-24 19:46:04 +0200

    fix the pattern condition for method annotation

--- src/main/kotlin/com/github/alaanor/candid/completion/MethodAnnotationCompletion.kt
+++ src/main/kotlin/com/github/alaanor/candid/completion/MethodAnnotationCompletion.kt
@@ -5,16 +5,22 @@ import com.github.alaanor.candid.psi.CandidMethType
 import com.intellij.codeInsight.completion.InsertHandler
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.patterns.ElementPattern
+import com.intellij.patterns.PatternCondition
 import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.psi.PsiElement
 import com.intellij.psi.TokenType
+import com.intellij.util.ProcessingContext
 
 class MethodAnnotationCompletion : CandidBasicCompletion() {
     override val elementPattern: ElementPattern<out PsiElement> = psiElement()
         .afterSiblingSkipping(
             psiElement(TokenType.WHITE_SPACE),
             psiElement(CandidActor::class.java)
-                .withLastChild(psiElement(CandidMethType::class.java))
+                .with(object : PatternCondition<CandidActor>("bruh") {
+                    override fun accepts(t: CandidActor, context: ProcessingContext?): Boolean {
+                        return t.lastChild is CandidMethType
+                    }
+                })
         )
 
     override val tokenList: List<String> = listOf("query", "oneway")
