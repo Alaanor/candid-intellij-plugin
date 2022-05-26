1dfd8f29f9e4570a337b9db1471776c9ca38d780
Maxime Bonvin <max.bonvin@gmail.com>
2022-05-26 16:29:13 +0200

    Show the correct icon for type reference

--- src/main/kotlin/com/github/alaanor/candid/psi/mixin/CandidIdentifierReferenceMixin.kt
+++ src/main/kotlin/com/github/alaanor/candid/psi/mixin/CandidIdentifierReferenceMixin.kt
@@ -1,14 +1,19 @@
 package com.github.alaanor.candid.psi.mixin
 
+
+import com.github.alaanor.candid.icon.CandidIcons
 import com.github.alaanor.candid.psi.CandidIdentifierReference
 import com.github.alaanor.candid.psi.primitive.CandidElementBase
 import com.github.alaanor.candid.reference.CandidTypeReference
 import com.intellij.lang.ASTNode
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiReference
+import javax.swing.Icon
 
 abstract class CandidIdentifierReferenceMixin(node: ASTNode) : CandidElementBase(node), CandidIdentifierReference {
     override fun getReference(): PsiReference? {
         return CandidTypeReference(this, TextRange.create(0, textLength))
     }
+
+    override fun getIcon(flags: Int): Icon = CandidIcons.Type
 }
