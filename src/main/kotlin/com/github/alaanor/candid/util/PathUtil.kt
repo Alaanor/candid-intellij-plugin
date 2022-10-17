7ab6346d2588e8486e5cf33e2543628b9f1ece16
Maxime Bonvin <max.bonvin@gmail.com>
2022-10-17 18:32:49 +0200

    only relativize when the root is the same

--- src/main/kotlin/com/github/alaanor/candid/util/PathUtil.kt
+++ src/main/kotlin/com/github/alaanor/candid/util/PathUtil.kt
@@ -7,21 +7,27 @@ import java.nio.file.Paths
 fun PsiElement.filePath(): String = containingFile.originalFile.virtualFile.path
 fun PsiElement.filePathDirectory(): String = containingFile.originalFile.virtualFile.parent.path
 
-fun PsiElement.projectFilePath(): String {
-    return Paths.get(project.basePath)
-        .relativize(Paths.get(containingFile.originalFile.virtualFile.path))
-        .toString()
+fun PsiElement.projectFilePath(): String? {
+    val from = Paths.get(project.basePath)
+    val to = Paths.get(containingFile.originalFile.virtualFile.path)
+    if (from.root != to.root)
+        return null
+    return from.relativize(to).toString()
 }
 
-fun PsiElement.getRelativePath(psiElement: PsiElement): String {
-    return Paths.get(filePathDirectory())
-        .relativize(Paths.get(psiElement.filePath()))
-        .toString()
+fun PsiElement.getRelativePath(psiElement: PsiElement): String? {
+    val from = Paths.get(filePathDirectory())
+    val to = Paths.get(psiElement.filePath())
+    if (from.root != to.root)
+        return null
+    return from.relativize(to).toString()
 }
 
-fun PsiElement.getRelativePath(virtualFile: VirtualFile): String {
-    return Paths.get(filePathDirectory())
-        .relativize(Paths.get(virtualFile.path))
-        .toString()
+fun PsiElement.getRelativePath(virtualFile: VirtualFile): String? {
+    val from = Paths.get(filePathDirectory())
+    val to = Paths.get(virtualFile.path)
+    if (from.root != to.root)
+        return null
+    return from.relativize(to).toString()
 }
 
